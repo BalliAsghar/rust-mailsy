a04522ba98a99b3ca7ceef01e7f23270431ed877
balliAsghar <ballihacker@outlook.com>
2022-04-28 04:59:05 +0100

    structs move its own dir

---
+++ src/libs/structs.rs
@@ -0,0 +1,45 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[allow(dead_code)]
+pub(crate) struct Config {
+    pub token: String,
+    pub email_address: String,
+    pub account_creation_date: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct DomainResponse {
+    #[serde(rename = "hydra:member")]
+    pub domain: Vec<Domain>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct Domain {
+    pub domain: String,
+    #[serde(rename = "isActive")]
+    pub is_active: bool,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct ErrorResponse {
+    pub violations: Vec<Violations>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct Violations {
+    pub message: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct AuthResponse {
+    pub token: String,
+    pub id: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct AuthError {
+    pub code: u32,
+    pub message: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct AccountResponse {
+    pub address: String,
+    #[serde(rename = "createdAt")]
+    pub created_at: String,
+}
