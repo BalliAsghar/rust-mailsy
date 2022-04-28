b1512120b27281310084c1f6644adb3a1be83682
balliAsghar <ballihacker@outlook.com>
2022-04-28 04:58:50 +0100

    layout change

--- src/main.rs
+++ src/main.rs
@@ -2,64 +2,14 @@ use clap::Command;
 use colored::*;
 use dirs;
 use fastrand;
-use serde::{Deserialize, Serialize};
 #[allow(unused_imports)]
 use serde_json::json;
-use std::{
-    fs::File,
-    future::Future,
-    io::{Read, Write},
-    path::Path,
-};
-use tokio::runtime::Runtime;
+use std::{fs::File, io::Read, path::Path};
+mod libs;
 
 // CONSTANTS
 #[allow(dead_code)]
 const API_ENDPOINT: &str = "https://api.mail.tm";
-
-#[derive(Debug, Deserialize, Serialize)]
-#[allow(dead_code)]
-struct Config {
-    token: String,
-    email_address: String,
-    account_creation_date: String,
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct DomainResponse {
-    #[serde(rename = "hydra:member")]
-    domain: Vec<Domain>,
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct Domain {
-    domain: String,
-    #[serde(rename = "isActive")]
-    is_active: bool,
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct ErrorResponse {
-    violations: Vec<Violations>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Violations {
-    message: String,
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct AuthResponse {
-    token: String,
-    id: String,
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct AuthError {
-    code: u32,
-    message: String,
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct AccountResponse {
-    address: String,
-    #[serde(rename = "createdAt")]
-    created_at: String,
-}
 
 fn main() {
     let matches = Command::new("mail")
@@ -75,16 +25,16 @@ fn main() {
 
     match matches.subcommand() {
         Some(("gen", _)) => {
-            asyn_runtime(gen());
+            libs::utils::asyn_runtime(gen());
         }
         Some(("del", _)) => {
-            asyn_runtime(del());
+            libs::utils::asyn_runtime(del());
         }
         Some(("mail", _)) => {
-            asyn_runtime(mail());
+            libs::utils::asyn_runtime(mail());
         }
         Some(("read", _)) => {
-            asyn_runtime(read());
+            libs::utils::asyn_runtime(read());
         }
         _ => {} // Either no subcommand or one not tested for...
     }
@@ -97,7 +47,7 @@ async fn gen() {
     // if file does not exist, then create the file
     if !config_file_path.exists() {
         let mut file = File::create(&config_file_path).unwrap();
-        create_config_file(&mut file).await;
+        libs::utils::create_config_file(&mut file).await;
     }
 
     // read the file
@@ -108,7 +58,7 @@ async fn gen() {
     file.read_to_string(&mut contents).unwrap();
 
     // deserialize the file
-    let config: Config = toml::from_str(&contents).unwrap();
+    let config: libs::structs::Config = toml::from_str(&contents).unwrap();
 
     // if the token is not epmty, then we have a valid config file.
     if !config.token.is_empty() {
@@ -124,26 +74,8 @@ async fn del() {}
 #[allow(dead_code)]
 async fn mail() {}
 
-async fn read() {
-    // read the output.json file
-    let mut file = std::fs::File::open("output.json").unwrap();
+async fn read() {}
 
-    // convert the file to json using serde
-    let mut contents = String::new();
-
-    file.read_to_string(&mut contents).unwrap();
-
-    let json: serde_json::Value = serde_json::from_str(&contents).unwrap();
-
-    // pretty print the json
-    println!("{}", serde_json::to_string_pretty(&json).unwrap());
-}
-
-fn asyn_runtime(async_fn: impl Future<Output = ()>) {
-    let rt = Runtime::new().unwrap();
-    rt.block_on(async_fn);
-}
-#[allow(dead_code)]
 async fn genrate_new_email_address(_file: &mut File) {
     // create the client
     let client = reqwest::Client::new();
@@ -155,7 +87,7 @@ async fn genrate_new_email_address(_file: &mut File) {
     let response = client.get(endpoint).send().await.unwrap();
 
     // deserialize the response
-    let domain_response: DomainResponse = response.json().await.unwrap();
+    let domain_response: libs::structs::DomainResponse = response.json().await.unwrap();
     // grab the first domain
     let domain = &domain_response.domain[0].domain;
     // generate a random address
@@ -185,26 +117,12 @@ async fn genrate_new_email_address(_file: &mut File) {
     // check if the request was not successful
     if !response.status().is_success() {
         // deserialize the response
-        let error_response: ErrorResponse = response.json().await.unwrap();
+        let error_response: libs::structs::ErrorResponse = response.json().await.unwrap();
         println!("{}", error_response.violations[1].message.red());
         return;
     }
 
     // deserialize the response
-    let auth_response: AccountResponse = response.json().await.unwrap();
-    // TODO: write the response to a file for later use
-}
-
-async fn create_config_file(file: &mut File) {
-    let config = Config {
-        token: "".to_string(),
-        email_address: "".to_string(),
-        account_creation_date: "".to_string(),
-    };
-    let toml = toml::to_string(&config).unwrap();
-
-    // write to file
-    file.write_all(toml.as_bytes()).unwrap();
-
-    println!("{}", "Config file created".green());
+    let auth_response: libs::structs::AccountResponse = response.json().await.unwrap();
+    // TODO: Solve the issue of missing field 'token'
 }
